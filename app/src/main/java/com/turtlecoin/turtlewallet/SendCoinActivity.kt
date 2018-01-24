0d2edab20d4c897e672586f23c71a73349821a1e
Marshall Mathers <mariomario.mb@gmail.com>
2018-01-24 03:40:06 +0100

    QR scanning added
    Added scanning functionailty, only accepts TRTL Addresses

--- app/src/main/java/com/turtlecoin/turtlewallet/SendCoinActivity.kt
+++ app/src/main/java/com/turtlecoin/turtlewallet/SendCoinActivity.kt
@@ -1,8 +1,13 @@
 package com.turtlecoin.turtlewallet
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.Intent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_send_coin.*
+import com.google.zxing.integration.android.IntentIntegrator
 
 class SendCoinActivity : AppCompatActivity() {
 
@@ -14,7 +19,29 @@ class SendCoinActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    // Open QR Intent
     fun readQRCodeOnClick(view: View) {
-        // TODO: Start intent of QR code reader
+        val integrator = IntentIntegrator(this@SendCoinActivity)
+        integrator.initiateScan()
+    }
+
+    // Deal with QR Result
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent)
+        if (scanResult != null) {
+            val contents = intent.getStringExtra("SCAN_RESULT")
+            if(isTRTLAddress(contents)) {
+                et_address.setText(contents)
+            } else {
+                Toast.makeText(this, R.string.qr_scan_wrong, Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, R.string.qr_scan_failed, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Criteria for a TRTL Address
+    private fun isTRTLAddress(address: String): Boolean {
+        return (address.length == 99 && address.startsWith("TRTL"))
     }
 }
